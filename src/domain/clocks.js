901cbe78a1b59ee07907e9f1cfecb56f2fd20608
Bertrand Bougon <bertrand.bougon@gmail.com>
2019-06-30 09:41:09 +0200

    Refacto

--- src/domain/clocks.js
+++ src/domain/clocks.js
@@ -4,34 +4,42 @@ let ZoneId = require('js-joda').ZoneId;
 
 function Clocks(clock) {
     this._currentDateTime = ZoneCurrentDateTime.now(clock);
+
+    function currentDateTime(zoneId) {
+
+    }
 }
 
+Clocks.prototype.sameInstant = function (zoneId) {
+    return this._currentDateTime.sameInstant(zoneId);
+};
+
 Clocks.prototype.tokyo = function () {
-    return this._currentDateTime.sameInstant(ZoneId.of("Asia/Tokyo"));
+    return this.sameInstant(ZoneId.of("Asia/Tokyo"));
 };
 
 Clocks.prototype.moscow = function () {
-    return this._currentDateTime.sameInstant(ZoneId.of("Europe/Moscow"));
+    return this.sameInstant(ZoneId.of("Europe/Moscow"));
 };
 
 Clocks.prototype.paris = function () {
-    return this._currentDateTime.sameInstant(ZoneId.of('Europe/Paris'));
+    return this.sameInstant(ZoneId.of('Europe/Paris'));
 };
 
 Clocks.prototype.london = function () {
-    return this._currentDateTime.sameInstant(ZoneId.of('Europe/London'));
+    return this.sameInstant(ZoneId.of('Europe/London'));
 };
 
 Clocks.prototype.newYork = function () {
-    return this._currentDateTime.sameInstant(ZoneId.of('America/New_York'));
+    return this.sameInstant(ZoneId.of('America/New_York'));
 };
 
 Clocks.prototype.losAngeles = function () {
-    return this._currentDateTime.sameInstant(ZoneId.of('America/Los_Angeles'));
+    return this.sameInstant(ZoneId.of('America/Los_Angeles'));
 };
 
 Clocks.prototype.saoPaulo = function () {
-    return this._currentDateTime.sameInstant(ZoneId.of('America/Sao_Paulo'));
+    return this.sameInstant(ZoneId.of('America/Sao_Paulo'));
 };
 
 Clocks.prototype.all = function () {
