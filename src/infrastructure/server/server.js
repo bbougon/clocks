da36cf73febfd654a6c36450869d07329609b13f
Bertrand Bougon <bertrand.bougon@gmail.com>
2019-07-02 06:59:05 +0200

    add cors

--- src/infrastructure/server/server.js
+++ src/infrastructure/server/server.js
@@ -1,5 +1,6 @@
 const ClockResource = require('./../../web/resources/clock-resource');
 var express = require("express");
+var cors = require('cors');
 
 function Server(port) {
     this._port = port;
@@ -7,6 +8,7 @@ function Server(port) {
 
 Server.prototype.start = function () {
     const app = express();
+    app.use(cors());
     app.listen(this._port, () => {
         console.log('{Server running on port ' + this._port + '}');
     });
